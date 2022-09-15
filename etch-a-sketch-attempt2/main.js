57644f79a1147e11eb9ca4b9096e655db7d6172e
Pranit <100540636+pranit-garg@users.noreply.github.com>
2022-09-15 18:41:45 +0800

    fix mouseover and grid setting button

--- etch-a-sketch-attempt2/main.js
+++ etch-a-sketch-attempt2/main.js
@@ -1,12 +1,39 @@
-let numberOfBoxesInGrid = 16;
-
 // setting the initial board and 
-let board = document.querySelector(".board")
-board.style.gridTemplateColumns = `repeat(${numberOfBoxesInGrid}, 1fr)`
-board.style.gridTemplateRows =  `repeat(${numberOfBoxesInGrid}, 1fr)`
+function populateBoard(BoxesInGrid) {
+    let board = document.querySelector(".board")
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${BoxesInGrid}, 1fr)`
+    board.style.gridTemplateRows =  `repeat(${BoxesInGrid}, 1fr)`
 
-for (let i = 0; i < numberOfBoxesInGrid * numberOfBoxesInGrid; i++) {
-    let square = document.createElement("div");
-    square.style.backgroundColor = 'yellow';
-    board.insertAdjacentElement("beforeend", square);
+        for (let i = 0; i < BoxesInGrid * BoxesInGrid; i++) {
+            let square = document.createElement("div");
+            square.style.backgroundColor = 'black';
+            square.addEventListener("mouseover", () => {square.style.backgroundColor = 'white';});
+            board.insertAdjacentElement("beforeend", square);
+        }
 }
+
+function changeBoard(input) {
+    populateBoard(input);
+}
+
+
+
+// adding event listener to set grid size
+let gridButton = document.querySelector(".setGridSizeButton");
+gridButton.addEventListener("click", setGridSize);
+
+function setGridSize() {
+    let requestedGridSize = prompt("What would you like the grid size to be?")
+    if (requestedGridSize > 0 && requestedGridSize < 100) {
+        changeBoard(requestedGridSize);
+
+    } else {
+        alert("Please enter an integer between 0 and 100")
+    }
+}
+
+
+
+populateBoard(16);
